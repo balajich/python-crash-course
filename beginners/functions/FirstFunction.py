16cc1c01200b96f685b22bbb1741cf7a531fa930
balaji <balaji.chopparapu@gmail.com>
2019-06-15 09:42:27 +0530

    Functions tutorials

---
+++ beginners/functions/FirstFunction.py
@@ -0,0 +1,16 @@
+'''
+Contains examples of functions
+'''
+
+
+def hello_world():
+    """
+    Function with our arguments and return type
+    :return:
+    """
+    print('Hello World')
+
+
+if __name__ == '__main__':
+    # Calling function
+    hello_world()
