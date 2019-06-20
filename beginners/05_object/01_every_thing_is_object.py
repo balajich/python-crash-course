fa5b9897347d098c92d9150c58c57635a4f5e208
balaji <balaji.chopparapu@gmail.com>
2019-06-20 18:49:58 +0530

    Organized Sections

---
+++ beginners/05_object/01_every_thing_is_object.py
@@ -0,0 +1,22 @@
+'''
+Everything is object in python
+'''
+
+if __name__ == "__main__":
+    # Primitives are object
+    # int is object
+    print(type(10))
+
+    # float is object
+    print(type(10.5))
+
+    # boolean is object
+    print(type(True))
+
+    # None is object
+    print(type(None))
+
+    # even modules are objects
+    import sys
+
+    print(type(sys))
