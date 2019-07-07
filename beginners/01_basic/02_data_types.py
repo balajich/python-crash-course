62ebfe27152f8dd1885da03eea19f2f541a5b9c0
balaji <balaji.chopparapu@gmail.com>
2019-07-07 22:02:16 +0530

    Renamed Hireachty

--- beginners/01_basic/02_data_types.py
+++ beginners/01_basic/02_data_types.py
@@ -1,7 +1,12 @@
 # integer data type
+# 42 bit arbitrary precision integer
+# unlimited precision signed integer
 print(type(10))
 
 # float data type
+# 64-bit floating point numbers
+# 53 bits of binary precision
+# 15 to 16 bits of decimal precision
 print(type(10.5))
 
 # String data type
@@ -11,4 +16,5 @@ print(type('Hello'))
 print(type(True))
 
 # None data type
+# the null object
 print(type(None))
---
+++ beginners/01_basic/05_Relational_Operators.py
@@ -0,0 +1,17 @@
+# value equality / equivalence
+print(1 == 1)
+
+# value inequality / inequivalence
+print(1 != 1)
+
+# less-than
+print(1 < 2)
+
+# greater-than
+print(1 > 2)
+
+# less-than or equal to
+print(1 <= 2)
+
+# greater-than or equal to
+print(1 >= 2)
