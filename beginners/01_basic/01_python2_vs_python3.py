e093b6527889cd96d4ebd768f3f4d162d01ca7e3
balaji <balaji.chopparapu@gmail.com>
2019-07-07 18:36:52 +0530

    Renamed Hireachty

---
+++ beginners/01_basic/01_python2_vs_python3.py
@@ -0,0 +1,5 @@
+print("This is python3 print function")
+
+# Doesnt work in python 3
+# Python 3 is not backward compatible
+# print "This is python 2 print"
---
+++ beginners/01_basic/02_data_types.py
@@ -0,0 +1,14 @@
+# integer data type
+print(type(10))
+
+# float data type
+print(type(10.5))
+
+# String data type
+print(type('Hello'))
+
+# Bool data type
+print(type(True))
+
+# None data type
+print(type(None))
