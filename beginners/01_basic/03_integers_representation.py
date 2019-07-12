59a014749c20c291b7abc4697754653430e87426
balaji <balaji.chopparapu@gmail.com>
2019-07-12 19:37:08 +0530

    Renamed Hireachty

---
+++ beginners/01_basic/03_integers_representation.py
@@ -0,0 +1,21 @@
+'''Representing Integers'''
+# Simple Integer
+print(2)
+
+# Generally Integers are with base 10
+print(2)
+
+# Binary or Base 2 integer representation
+print(0b10)  # This is numeric 2 with base 2
+
+# Octal or Base 8
+print(0o10)  # This is numeric 8 with base 8
+
+# HexaDecimal or Base 16
+print(0x10)  # This is numeric 16 with base 16
+
+print(0x0F)  # This is numeric 15 with base 16
+
+# Underscores in Numbers
+# When writing long number you can group the with underscores
+print(143_000_000)  # Distance between earth and sun in kilometers
