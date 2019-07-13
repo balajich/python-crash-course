0d97d8ff38d64b53e29ff81a6ae7fbdeddf6943e
balaji <balaji.chopparapu@gmail.com>
2019-07-13 10:45:46 +0530

    bool supports

--- beginners/01_basic/12_bool_conversion.py
+++ beginners/01_basic/12_bool_conversion.py
@@ -1,10 +1,18 @@
 '''Convert one data type to other'''
 
-# Convert String to int
-print(int('10'))
+# Conversion of int to bool
+# Bool of none zero is true
+print(bool(0))  # False
+# Rest is true
+print(bool(1))
+print(bool(-1))
+print(bool(67))
 
-# Convert String to float
-print(float('10.2'))
+# Conversion of float to bool
+print(bool(0.0))  # False
+# Rest is true
+print(bool(1.0))
+print(bool(-0.1))
 
 # Convert String to bool
 # bool of non-empty is true
@@ -15,3 +23,8 @@ print(bool(''))  # This is False
 print(bool('True'))
 print(bool('False'))  # Pay attention this is also True
 print(bool(None))  # This is false
+
+# Conversion of collection to bool
+# Only empty collection is false
+print(bool([]))  # Empty list is false
+print(bool([1, 2, 3]))  # This is true
---
+++ beginners/01_basic/14_Conditional_if_statements_Statements.py
@@ -0,0 +1,26 @@
+'''IF statement
+if expr:
+    print("expr is True")
+expr is converted to bool using bool() constructor
+
+
+if expr:
+    print("expr is True")
+else:
+    print("expr is False”)
+
+if expr:
+    print("expr is True")
+elif expr:
+    print("expr is True”)
+else:
+    print("expr is False”)
+
+'''
+
+age = 60
+
+if age > 60:
+    print('Senior Citizen')
+else:
+    print('Not Senior Citizen')
